bb3ba244af19e10575929da9d3c5097a2ab35bd4
phanhoangnhan91@gmail.com <phanhoangnhan91@gmail.com@21acf340-8a49-ead0-8753-cd6adfc59f2a>
2014-08-21 14:35:30 +0000

    commit fix loi phuong thuc sinh key chi chạy được trên máy cần tạo key( chưa text)

--- trunk/GennerateKey/GennerateKey/Form1.Designer.cs
+++ trunk/GennerateKey/GennerateKey/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.txtProducKey = new System.Windows.Forms.TextBox();
             this.txtLicenceKey = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -67,17 +68,29 @@
             this.txtLicenceKey.Size = new System.Drawing.Size(195, 20);
             this.txtLicenceKey.TabIndex = 3;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(283, 121);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.txtLicenceKey);
             this.Controls.Add(this.txtProducKey);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,6 +102,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtProducKey;
         private System.Windows.Forms.TextBox txtLicenceKey;
+        private System.Windows.Forms.Button button1;
     }
 }
 
